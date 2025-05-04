c0a5edb36b16482875cc22f9e7d74c1eaff144f9
Daniel Vedamuthu <daniel.pravin@gmail.com>
2025-05-03 22:38:17 -0500

    Added Title and changes text

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,10 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title:'Flutter Project',
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Hello, Flutter!'),
         ),
       ),
     );
